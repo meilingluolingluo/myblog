1965e6604ae974a6c060014962d437424dffdcd2
mll <2538413679@qq.com>
2024-10-04 16:28:06 +0800

    环境搭建

---
+++ weblog-module-admin/src/test/java/cn/mll/weblog/admin/WeblogMoudleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mll.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogMoudleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
