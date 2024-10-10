03cabe497af469aba101b675110dd200d43fb6cb
mll <2538413679@qq.com>
2024-10-10 19:34:02 +0800

    自定义 Jackson 序列化、反序列化

--- weblog-web/src/main/java/com/mll/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/mll/weblog/web/controller/TestController.java
@@ -3,8 +3,10 @@ package com.mll.weblog.web.controller;
 import com.mll.weblog.common.aspect.ApiOperationLog;
 import com.mll.weblog.common.enums.ResponseCodeEnum;
 import com.mll.weblog.common.exception.BizException;
+import com.mll.weblog.common.utils.JsonUtil;
 import com.mll.weblog.common.utils.Response;
 import com.mll.weblog.web.model.User;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.validation.FieldError;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.stream.Collectors;
 
 /**
@@ -28,8 +34,17 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public Response test(@RequestBody @Validated User user) {
-        return Response.success();
+    @ApiOperation(value = "测试接口")
+    public Response<User> test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
     }
 
 
--- weblog-web/src/main/java/com/mll/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/mll/weblog/web/model/User.java
@@ -4,6 +4,9 @@ package com.mll.weblog.web.model;
 import lombok.Data;
 
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * @Title: User
@@ -31,4 +34,10 @@ public class User {
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")  // 注解确保邮箱格式正确
     private String email;
+    // 创建时间
+    private LocalDateTime createTime;
+    // 更新日期
+    private LocalDate updateDate;
+    // 时间
+    private LocalTime time;
 }
